317ce28525a90c49677b77398a5f1275678c64ff
Robert Kuramshin <robert@sensorscall.com>
2018-11-08 03:18:09 -0500

    update double perovskites example

--- KRR_predict.m
+++ KRR_predict.m
@@ -1,12 +1,11 @@
 %KRR Predict
 %Robert Kuramshin
-function [predicted]=KRR_predict(x_train,y_train,x_test,K,k,lambda)
+function [y_predicted]=KRR_predict(x_train,y_train,x_test,K,k,lambda)
     N_test = length(x_test);
     N_train = length(x_train);
 
-    predicted=zeros(N_test,1);
-
+    y_predicted=zeros(N_test,1);
     for i=1:N_test
-        predicted(i,1)= y_train'*((K+ lambda*eye(N_train))\k(i,:)');
+        y_predicted(i,1)= y_train'*((K+ lambda*eye(N_train))\k(i,:)');
     end
 end
