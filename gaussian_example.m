0c750f90e5c9343ffd58e118ee5b13f0ff1bd4e2
Robert Kuramshin <robert@sensorscall.com>
2018-11-08 01:57:40 -0500

    completed example and wrote a walkthrough

--- gaussian_example.m
+++ gaussian_example.m
@@ -1,5 +1,5 @@
-% Process Sample Dataset
-% double_perovskites_gap.csv
+% Gaussian Example
+% Robert Kuramshin
 clear all;clc;
 
 data=zscore(csvread('gaussian_data.csv'));
@@ -37,18 +37,17 @@ for j=1:N_train
 end
 
 y_predicted_sample=zeros(N_train,1);
-y_predicted=zeros(N_test,1);
 
 lambda=0.1;
 
 for i=1:N_train
-    if mod(i,50) == 0
+    if mod(i,10) == 0
         fprintf('Training on Sample: %d of %d\n',i,N_train);
     end
     y_predicted_sample(i,1)= y_train'*((K_gauss+ lambda*eye(N_train))\K_gauss(i,:)');
 end
 
-in_sample_error = norm(y_predicted_sample-y_train)^2/N_train;
+in_sample_error = norm(y_predicted_sample-y_train)^2/N_train
 
 figure
 hs(1) = subplot(2,1,1);
@@ -74,12 +73,13 @@ for j=1:N_train
  end
 end
 
+y_predicted=zeros(N_test,1);
 
 for i=1:N_test
     y_predicted(i,1)= y_train'*((K_gauss+ lambda*eye(N_train))\k(i,:)');
 end
 
-out_sample_error = norm(y_predicted-y_test)^2/N_test;
+out_sample_error = norm(y_predicted-y_test)^2/N_test
 
 hs(2) = subplot(2,1,2);
 %out of sample
